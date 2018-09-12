d50b32f7cf1ae6825f04a00a313aa4c66766f5c0
code-project <dev@code-check.io>
2018-09-12 02:30:21 +0000

    Initial commit

---
+++ app/Main.php
@@ -0,0 +1,20 @@
+<?php namespace Codecheck;
+
+function run ($argc, $argv)
+{
+	if($argc !== 1) {
+        throw new Exception('No string is given.');
+	}
+    
+	$word = $argv[0];
+	
+    if ($word === 'World') {
+		echo 'Hello World!';
+	} else if($word === 'codecheck') {
+		echo 'Hello codecheck!';
+	} else if($word === '織田信長') {
+		echo 'Hello 織田信長!';
+	} else {
+		return;
+	}
+}

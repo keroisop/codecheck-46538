cc4bb986e1ba5d40939d6ce7ae754a40d3d3246e
code-project <dev@code-check.io>
2018-09-12 02:30:21 +0000

    Add new CLI related files

--- app/Main.php
+++ app/Main.php
@@ -2,19 +2,7 @@
 
 function run ($argc, $argv)
 {
-	if($argc !== 1) {
-        throw new Exception('No string is given.');
-	}
-    
-	$word = $argv[0];
-	
-    if ($word === 'World') {
-		echo 'Hello World!';
-	} else if($word === 'codecheck') {
-		echo 'Hello codecheck!';
-	} else if($word === '織田信長') {
-		echo 'Hello 織田信長!';
-	} else {
-		return;
-	}
+  foreach ($argv as $index=>$value) {
+    printf("argv[%s]: %s\n", $index, $value);
+  }
 }
